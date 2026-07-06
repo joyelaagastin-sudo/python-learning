7b30acb6739ec7aa77eb29f0b25361dd6217f89f
A Joyel Agastin <joyelaagastin@gmail.com>
2026-07-06 11:46:20 +0530

    something

---
+++ 1.variable_operators/practice.py
@@ -0,0 +1,48 @@
+# # 1
+# x = 5
+# y = 2
+
+# print("Addition:", x + y)
+# print("Subtraction:", x - y)
+# print("Division:", x / y)
+# print("XOR:", x ^ y)
+# print("Modulus:", x % y)
+# print("Floor Division:", x // y)
+
+# # 5
+# salary1=10000
+# employee="John"
+# print(salary1)
+# print(f"Employee Name: {employee}, Salary: {salary1}")
+# new_salary=salary1+2000
+# print(f"New Salary: {new_salary}")
+
+# # 7
+# num = 8
+
+# print("Last bit is", num & 1)
+
+# # 8
+# x = 5
+# y = 2
+
+# x, y = y, x
+
+# print("x =", x)
+# print("y =", y)
+
+# # 9
+# text = "hello"
+
+# print("a" in text)
+
+
+# # 10
+# a = 3
+# b = 5
+
+# a += 5
+# b += 5
+
+# print("a =", a)
+# print("b =", b)
