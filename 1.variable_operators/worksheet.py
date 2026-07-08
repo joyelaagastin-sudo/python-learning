e6969ff3c9961dbbe0829583d032d959c5520fef
A Joyel Agastin <joyelaagastin@gmail.com>
2026-07-08 11:59:14 +0530

    worksheet done

---
+++ 1.variable_operators/worksheet.py
@@ -0,0 +1,33 @@
+# # 1
+# donation =500
+# print(f"hi i have donated {donation} to the charity")
+
+# 2
+# print("\t\t$")
+# print("\t$\t$\t$")
+# print("$\t$\t$\t$\t$")
+
+# #3
+# a = int(input("enter the first number :"))
+# b = int(input("enter the second number :"))
+# print(f"sum of {a} and {b} is {a+b}")
+
+# #4
+# marks = input("Enter 5 subject marks by entering space between them: ").split()
+
+# maths = int(marks[0])
+# science = int(marks[1])
+# english = int(marks[2])
+# tamil = int(marks[3])
+# social = int(marks[4])
+
+# total = maths + science + english + tamil + social
+
+# print("Total Marks =", total)
+
+# #5
+# print("\nj\no\ny\ne\nl\n \na\ng\na\ns\nt\ni\nn")
+
+# #6
+# print("\"Be the living expression of GODS kindness -Mother Teresa\"")
+
