57c301e2f88fd662e4108e8ecece92f5109342a0
A Joyel Agastin <joyelaagastin@gmail.com>
2026-07-08 15:17:13 +0530

    loops

--- 1.variable_operators/worksheet.py
+++ 1.variable_operators/worksheet.py
@@ -13,22 +13,16 @@
 # print(f"sum of {a} and {b} is {a+b}")
 
 # #4
-# marks = input("Enter 5 subject marks by entering space between them: ").split()
+maths, science, english, tamil, social = map(int, input("Enter 5 subject marks separated by spaces: ").split())
 
-# maths = int(marks[0])
-# science = int(marks[1])
-# english = int(marks[2])
-# tamil = int(marks[3])
-# social = int(marks[4])
+total = maths + science + english + tamil + social
 
-# total = maths + science + english + tamil + social
+print("Total Marks =", total)
 
-# print("Total Marks =", total)
 
 # #5
 # print("\nj\no\ny\ne\nl\n \na\ng\na\ns\nt\ni\nn")
 
 # #6
 # print("\"Be the living expression of GODS kindness -Mother Teresa\"")
-
 
