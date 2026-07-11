c12f8ee798531e0fead0b1434668a3d5e0e9b760
A Joyel Agastin <joyelaagastin@gmail.com>
2026-07-11 14:22:30 +0530

    Worksheet 2A completed

---
+++ 2.Loops/worksheet.py
@@ -0,0 +1,78 @@
+# #1
+# num = int(input("Enter a number: "))
+
+# if num % 2 == 0:
+#     print("Even")
+# else:
+#     print("Odd")
+
+#2
+# marks = int(input("Enter your marks: "))
+
+# if marks >= 40:
+#     print("Pass")
+# else:
+#     print("Fail")
+
+#3
+# marks = int(input("Enter your marks: "))
+
+# if marks >= 90:
+#     print("Grade A")
+# elif marks >= 80:
+#     print("Grade B")
+# elif marks >= 70:
+#     print("Grade C")
+# elif marks >= 60:
+#     print("Grade D")
+# else:
+#     print("Grade F")
+
+#4
+
+# day = int(input("Enter a number (1-7): "))
+
+# if day == 1:
+#     print("Sunday")
+# elif day == 2:
+#     print("Monday")
+# elif day == 3:
+#     print("Tuesday")
+# elif day == 4:
+#     print("Wednesday")
+# elif day == 5:
+#     print("Thursday")
+# elif day == 6:
+#     print("Friday")
+# elif day == 7:
+#     print("Saturday")
+# else:
+#     print("Invalid Input")
+
+#5
+
+# age = int(input("Enter your age: "))
+# citizen = input("Are you an Indian citizen? (yes/no): ")
+
+# if age >= 18:
+#     if citizen == "yes":
+#         print("Eligible to vote")
+#     else:
+#         print("Not eligible (Not an Indian citizen)")
+# else:
+#     print("Not eligible (Age below 18)")
+
+# #6
+# balance = 10000
+# amount = int(input("Enter withdrawal amount: "))
+# pin = int(input("Enter PIN: "))
+
+# if balance >= amount:
+#     if pin == 7070:
+#         print("Withdrawal Successful")
+#         new_balance = balance - amount
+#         print("New Balance:", new_balance)
+#     else:
+#         print("Incorrect PIN")
+# else:
+#     print("Insufficient Balance")
