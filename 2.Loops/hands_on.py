37edb576e3cd57143e9363a2f4dab08b05e48201
A Joyel Agastin <joyelaagastin@gmail.com>
2026-07-11 18:37:18 +0530

    for loop concepts

--- 2.Loops/hands_on.py
+++ 2.Loops/hands_on.py
@@ -1,11 +1,24 @@
-pcb = int(input("Enter the PCB percentage: "))
-pcm = int(input("Enter the PCM percentage: "))
+# pcb = int(input("Enter the PCB percentage: "))
+# pcm = int(input("Enter the PCM percentage: "))
 
-if pcb == 100:
-    college = "Your ward has been selected in the Medical College."
-elif pcm == 100:
-    college = "Your ward has been selected in the Engineering College."
-else:
-    college = "Your ward has been selected in the Arts College."
+# if pcb == 100:
+#     college = "Your ward has been selected in the Medical College."
+# elif pcm == 100:
+#     college = "Your ward has been selected in the Engineering College."
+# else:
+#     college = "Your ward has been selected in the Arts College."
 
-print(college)
+# print(college)
+
+# for loop
+
+# for i in range(1, 11):
+#     print(i)           
+
+
+# 
+# total = 0
+# for i in range(1, 6):
+#     subject_mark = int(input("Enter the marks for subject : "))
+#     total += subject_mark
+# print("Total marks:", total)
