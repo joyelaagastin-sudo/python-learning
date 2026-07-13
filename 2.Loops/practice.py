4e8d9c5b30cb0d658e29b733be1fd43cb14f2a81
A Joyel Agastin <joyelaagastin@gmail.com>
2026-07-13 18:18:21 +0530

    loops practice

---
+++ 2.Loops/practice.py
@@ -0,0 +1,32 @@
+# practice 
+# # 1
+# for i in range(1, 16):
+#     print(i)
+
+# 2
+# for i in range(16,-1,-1):
+#     print(i)
+
+# 3
+# num = int(input("Enter a number: "))
+# for i in range(1, 13):
+#     print(f"{num} x {i} = {num * i}")
+    
+    
+    
+    
+ # 4
+# for i in range(1, 31):
+#     if i % 2 == 0:
+#         print(i)
+
+# 5
+
+# for i in range(1, 31):
+#     if i % 2 != 0:
+#         print(i)
+
+# 6
+
+
+    
--- 2.Loops/worksheet_for_loops.py
+++ 2.Loops/worksheet_for_loops.py
@@ -26,9 +26,9 @@
 
 # 5
 
-# n = int(input("Enter a number: "))
-# sum = 0
-# for i in range(1, n + 1):
-#     sum = sum + i
-# print("Sum =", sum)                  
+n = int(input("Enter a number: "))
+sum = 0
+for i in range(1, n + 1):
+    sum = sum + i
+print("Sum =", sum)                  
 
