b0bb5f609d795ed7dab1bfeff721ae75ef2aa870
A Joyel Agastin <joyelaagastin@gmail.com>
2026-07-13 09:47:55 +0530

    worksheet for for loop

---
+++ 2.Loops/worksheet_for_loops.py
@@ -0,0 +1,33 @@
+#1
+
+# for i in range(1, 11):
+#     print(i)
+
+#2
+
+# num = int(input("Enter a number: "))
+# for i in range(1, 11):
+#     print(f"{num} x {i} = {num * i}")
+
+
+# 3
+
+# for i in range(1, 21):
+#     if i == 13:
+#         break
+#     print(i)
+ 
+#  4
+
+# for i in range(1, 21):
+#     if i % 3 == 0:
+#         continue
+#     print(i)
+
+# 5
+
+# n = int(input("Enter a number: "))
+# sum = 0
+# for i in range(1, n + 1):
+#     sum = sum + i
+# print("Sum =", sum)
